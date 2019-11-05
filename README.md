74efb60d052a0be6bafb243b5036e51a2f0422d3
David Song <dhsong91@gmail.com>
2019-11-05 09:11:04 -0500

    score stops at playing to number

---
+++ README.md
@@ -0,0 +1,3 @@
+Application following the works of Colt Steele.
+Figuring out vanilla JS methods to combine the use
+of HTML and events from JS to enable logical functions.
--- app.js
+++ app.js
@@ -3,13 +3,25 @@ let p2Button = document.querySelector("#p2");
 let p1Display = document.querySelector("#p1Display");
 let p1Score = 0;
 let p2Score = 0;
+let gameOver = false;
+let winningScore = 5;
 
 p1Button.addEventListener("click", function(){
-    p1Score++;
-    p1Display.textContent = p1Score;
+    if(!gameOver){
+        p1Score++;
+        if(p1Score === winningScore){
+            gameOver = true;
+        }
+        p1Display.textContent = p1Score;
+    }
 });
 
 p2Button.addEventListener("click", function(){
-    p2Score++;
-    p2Display.textContent = p2Score;
+    if(!gameOver){
+        p2Score++;
+        if(p2Score === winningScore){
+            gameOver = true;
+        }
+        p2Display.textContent = p2Score;
+    }
 });
