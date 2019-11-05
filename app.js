ec9abede429e9d0492deb815735b521a4d2e614b
David Song <dhsong91@gmail.com>
2019-11-05 09:59:31 -0500

    Reset button works now

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let p1Button = document.querySelector("#p1");
 let p2Button = document.querySelector("#p2");
 let p1Display = document.querySelector("#p1Display");
+let resetButton = document.querySelector("#reset");
 let p1Score = 0;
 let p2Score = 0;
 let gameOver = false;
@@ -10,6 +11,7 @@ p1Button.addEventListener("click", function(){
     if(!gameOver){
         p1Score++;
         if(p1Score === winningScore){
+            p1Display.classList.add('winner')
             gameOver = true;
         }
         p1Display.textContent = p1Score;
@@ -20,8 +22,19 @@ p2Button.addEventListener("click", function(){
     if(!gameOver){
         p2Score++;
         if(p2Score === winningScore){
+            p2Display.classList.add('winner')
             gameOver = true;
         }
         p2Display.textContent = p2Score;
     }
+});
+
+resetButton.addEventListener("click", function(){
+    p1Score = 0;
+    p2Score = 0;
+    p1Display.textContent = 0;
+    p2Display.textContent = 0;
+    p1Display.classList.remove("winner");
+    p2Display.classList.remove("winner");
+    gameOver = false;
 });
